c45c7fb927d58ebdee0486a42cb02bb91f02ce4c
dongri <853523391@qq.com>
2016-03-25 09:36:36 +0800

    添加一个测试窗体

---
+++ TimeManagement/TimeManagement/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TearcherRoom tr = new TearcherRoom();
+            tr.ID = 1;
+            tr.TearcharName = "郭老师";
+            tr.TearcharAge = 28;
+            richTextBox1.Text = tr.ToString();
+        }
+    }
+    public class TearcherRoom
+    {
+        public int ID { set; get; }
+        public string TearcharName { get; set; }
+        public int TearcharAge { get; set; }
+    }
+}
